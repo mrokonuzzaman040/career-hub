55014b160219f27813e5a6182e744cd5c23a02e9
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-09-24 15:24:09 +0600

    Almost done

--- src/components/ApplyedJobs/ApplyedJobs.jsx
+++ src/components/ApplyedJobs/ApplyedJobs.jsx
@@ -6,12 +6,29 @@ const ApplyedJobs = () => {
     const jobs = useLoaderData();
 
     const [applyedJobs, setApplyedJobs] = useState([]);
+    const [displayJobs, setDisplayJobs] = useState([]);
+
+    const habdelFilter = (filter) => {
+        if(filter === 'All') {
+            setDisplayJobs(applyedJobs);
+        }
+        else if(filter === 'Remote') {
+            const remoteJobs = applyedJobs.filter(job => job.remote_or_onsite === 'Remote');
+            setDisplayJobs(remoteJobs);
+        }
+        else if(filter === 'OnSite') {
+            const onSiteJobs = applyedJobs.filter(job => job.remote_or_onsite === 'Onsite');
+            setDisplayJobs(onSiteJobs);
+        }
+
+    };
 
     useEffect(() => {
         const storedJobIds = getStoredJobApplications();
         if (jobs.length > 0) {
             const applyedJobs = jobs.filter(job => storedJobIds.includes(job.id));
             setApplyedJobs(applyedJobs);
+            setDisplayJobs(applyedJobs);
         }
     }, []);
     // console.log(applyedJobs);
@@ -19,12 +36,22 @@ const ApplyedJobs = () => {
         <div>
             <h2>Jobs you have applyed</h2>
             <h2>Total Applyed Jobs: {applyedJobs.length} </h2>
+            <div className="">
+                <details className="dropdown mb-10">
+                    <summary className="m-1 btn">open or close</summary>
+                    <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                        <li onClick={() =>habdelFilter('All') }><a>All</a></li>
+                        <li onClick={() =>habdelFilter('Remote') }><a>Remote</a></li>
+                        <li onClick={() =>habdelFilter('OnSite') }><a>OnSite</a></li>
 
-            <ul>
+                    </ul>
+                </details>
+            </div>
+            <div className="mb-10 p-5 bg-blue-200 border-blue-300 rounded-xl">
                 {
-                    applyedJobs.map(job =>
-                        <div className="mb-10 p-5 bg-blue-200 border-blue-300 rounded-xl">
-                            <div className="flex gap-10 w-auto ">
+                    displayJobs.map(job =>
+                        <div key={job.id} className="mb-10">
+                            <div  className="flex gap-10 mb-10">
                                 <div className="w-[96px]">
                                     <img src={job.logo} alt="" />
                                 </div>
@@ -37,13 +64,12 @@ const ApplyedJobs = () => {
                                         <p>{job.remote_or_onsite}</p>
                                     </div>
                                     <p>{job.location}</p>
-
                                 </div>
                             </div>
                         </div>
                     )
                 }
-            </ul>
+            </div>
         </div>
     );
 };
